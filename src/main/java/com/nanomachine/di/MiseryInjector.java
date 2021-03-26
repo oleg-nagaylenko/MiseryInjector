091fc91451f29d66d6d69c35b133a2945f812ffc
nan0machinesun <nan0machinesun@gmail.com>
2021-03-26 19:18:19 +0200

    init commit

---
+++ src/main/java/com/nanomachine/di/MiseryInjector.java
@@ -0,0 +1,13 @@
+package com.nanomachine.di;
+
+/**
+ * Hello world!
+ *
+ */
+public class MiseryInjector
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nanomachine/di/MiseryInjectorTest.java
@@ -0,0 +1,19 @@
+package com.nanomachine.di;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class MiseryInjectorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
